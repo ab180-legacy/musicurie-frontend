97b1575742fbdd5999d769137d4a24a956d33cb8
SungpilNam <iimplicit@gmail.com>
2015-02-18 17:09:35 +0900

    set-up bootstrap grid system and put dummy data to test ui

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,8 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,15 @@
  */
 angular.module('musicurieApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	var main = this;
+
+  	main.musics = [{
+  		title: "흩날리는 벚꽃처럼",
+  		artist: "류원경",
+  		album: "Purdue에서 1집"
+  	}, {
+  		title: "요요맨",
+  		artist: "NSP",
+  		album: "빌딩에서 사는자 2집"
+  	}]
   });
